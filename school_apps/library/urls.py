f9499e109b8e277706ee028975021c8b776225c7
Nirmal Pandey <nirmalpandey048@gmail.com>
2023-07-15 09:47:01 +0545

    Updated fines

--- school_apps/library/urls.py
+++ school_apps/library/urls.py
@@ -61,6 +61,12 @@ urlpatterns = [
     path('book_renew_detail/<int:pk>/', book_renew_detail, name='book_renew_detail'),
     path('del_book_renew/<int:pk>/', BookRenewDeleteView.as_view(), name='del_book_renew'),
     path('book_renew/<int:pk>/', book_renew, name='book_renew'),
+   # ------------------------------------ Fine Management url -----------------------------------------------
+     path('add_library_fine/', add_library_fine, name='add_library_fine'),
+     path('library_fine_list/', library_fine_list, name='library_fine_list'),
+
+    path('edit_library_fine/<int:pk>/', edit_library_fine, name='edit_library_fine'),
+    path('del_library_fine/<int:pk>/', LibraryFineDeleteView.as_view(), name='del_library_fine'),
 
     
     path('library-management/',library_home,name = 'library-management'),
