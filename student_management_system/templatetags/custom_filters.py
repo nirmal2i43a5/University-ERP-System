3507d6e27f0619e3ad20e8aadc2e72188b946211
Nirmal Pandey <nirmalpandey048@gmail.com>
2023-07-15 09:06:57 +0545

    create custom filter to make fetch dynamic date in template

---
+++ student_management_system/templatetags/custom_filters.py
@@ -0,0 +1,8 @@
+from django import template
+from datetime import datetime, timedelta
+
+register = template.Library()
+
+@register.filter
+def days_until_expiry(expiry_date):
+    return (expiry_date - datetime.today().date()).days
